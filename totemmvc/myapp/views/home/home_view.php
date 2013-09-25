d43b832ea3f1eee5d9d92cb42851123ac9446aa8
Chris Mccreadie <chris@cjmcsoftware.co.uk>
2013-09-25 06:01:02 +0100

    typos

--- totemmvc/myapp/views/home/home_view.php
+++ totemmvc/myapp/views/home/home_view.php
@@ -44,14 +44,14 @@
   <div class="container">
     <div class="span12">
 		<div class="well">
-			Welcome, thank your taking a look at TotemMVC.
+			Welcome, thankyou for taking a look at TotemMVC.
 		</div>
 		<div class="span10">
 			<p>
 			TotemMVC is a lightweight MVC based on the original work of TinyMVC.
 			</p>
 			<p>
-				It was developed internally to get up and running fast with our CMS Platfrom (Mojag).  It does a lot of the bootstraping for you such removing the index.php in you ht access file or making sure your assets are all in a static directory.  Some of the things we do are obvious and none of it hard it's just, well damn convient to have a modern framework that has all the resoucres you need to get up and running with 0 effort (we like 0 effort at Totem).
+It was developed internally to get up and running fast with our CMS Platform (Mojag).  It does a lot of the bootstrapping for you such removing the index.php in you ht access file or making sure your assets are all in a static directory.  Some of the things we do are obvious and none of it hard it's just, well damn convenient to have a modern framework that has all the resources you need to get up and running with 0 effort (we like 0 effort at Totem).
 				</p>
 				<p>
 					it is not for everyone but if it's for you enjoy.
